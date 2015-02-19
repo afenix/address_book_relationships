09d6a2f7d0705fb06c23ef0a084299baebbc5654
J. TURNER <turner.j6@gmail.com>
2015-02-19 13:38:19 -0800

    Update JQuery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,27 @@
 $(document).ready(function() {
-  $("#add-address").click(function() {
-    $("#new-addresses").append( '<div class="new-address">' +
-                                  '<div class="form-group">' +
-                                    '<label for="new-street">Street: </label>' +
-                                    '<input type="text" id="new-street" class="form-control">' +
-                                  '</div>' +
+ $("#add-address").click(function() {
+   $("#new-addresses").append( '<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street: </label>' +
+                                   '<input type="text" id="new-street" class="form-control">' +
+                                 '</div>' +
 
-                                  '<div class="form-group">' +
-                                    '<label for="new-city">City: </label>' +
-                                    '<input type="text" id="new-city" class="form-control">' +
-                                  '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City: </label>' +
+                                   '<input type="text" id="new-city" class="form-control">' +
+                                 '</div>' +
 
-                                  '<div class="form-group">' +
-                                    '<label for="new-state">State: </label>' +
-                                    '<input type="text" id="new-state" class="form-control">' +
-                                  '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-state">State: </label>' +
+                                   '<input type="text" id="new-state" class="form-control">' +
+                                 '</div>' +
 
-                                  '<div class="form-group">' +
-                                    '<label for="new-zip">Zip: </label>' +
-                                    '<input type="text" id="new-zip" class="form-control">' +
-                                  '</div>' +
-                                '</div>');
-  });
-
+                                 '<div class="form-group">' +
+                                   '<label for="new-zip">Zip: </label>' +
+                                   '<input type="text" id="new-zip" class="form-control">' +
+                                 '</div>' +
+                               '</div>');
+ });
 
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
@@ -30,24 +29,24 @@ $(document).ready(function() {
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-    var inputtedStreet = $("input#new-street").val();
-    var inputtedCity = $("input#new-city").val();
-    var inputtedState = $("input#new-state").val();
-    var inputtedZip = $("input#new-zip").val();
+
 
     var newContact = { firstName: inputtedFirstName,
                        lastName: inputtedLastName,
-                       street: inputtedStreet,
-                       city: inputtedCity,
-                       state: inputtedState,
-                       zip: inputtedZip,
-                       fullAddress: function() {
-                         return(this.street + "\n" +
-                                this.city + ", " +
-                                this.state + "  " +
-                                this.zip);
-                       }
+                       addresses: []
                       };
+
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input#new-street").val();
+      var inputtedCity = $(this).find("input#new-city").val();
+      var inputtedState = $(this).find("input#new-state").val();
+      var inputtedZip = $(this).find("input#new-zip").val();
+      var newAddress = { street: inputtedStreet,
+                         city: inputtedCity,
+                         state: inputtedState,
+                         zip: inputtedZip
+                       };
+    });
 
     $("ul#contacts").append("<li><span class='contact'>"
                              + newContact.firstName
@@ -67,6 +66,5 @@ $(document).ready(function() {
       $(".last-name").text(newContact.lastName);
       $(".address").text(newContact.fullAddress());
     });
-
   });
 });
